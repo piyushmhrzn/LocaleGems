328d825315d40a82f941323c0c93cb8e6f74abd2
Gunjan Rajput <Gunjan@Apples-MacBook-Air.local>
2025-01-29 23:56:30 -0500

    database connection established

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,13 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
 
+
+
 dotenv.config();
 
 const app = express();
+
+
 app.use(cors());
 app.use(express.json());
 
