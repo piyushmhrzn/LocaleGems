d756d888f02b1c5fe2596110eeb574218b7da4d0
Nidhi <0211nidhi@gmail.com>
2025-01-30 22:20:05 -0500

    implementing navigations, worked along with simiya to implent it

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import TestComponent from './components/TestComponent';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Explore from "./pages/explore";
+import Blogs from "./pages/blog";
+import About from "./pages/about";
+import Contact from "./pages/contact";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <TestComponent />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/explore">Explore</a></li>
+          <li><a href="/blogs">Blogs</a></li>
+          <li><a href="/about">About Us</a></li>
+          <li><a href="/contact">Contact Us</a></li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
